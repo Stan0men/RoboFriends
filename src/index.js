7c99300cc427bdac9a41b4068bacb6a02fa2afa8
Stan0men <stan0men@yahoo.com>
2018-06-29 22:06:31 +0300

    6.3 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import CardList from './CardList';
 import 'tachyons';
-import {robots} from './robots';
+
 
 ReactDOM.render(
-<CardList robots={robots}/>,
+<App/>,
 document.getElementById('root'));
 registerServiceWorker();
